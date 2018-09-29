1d35b744bcf482a2c275f4cfc7def2ca2919500f
SateeshDara <42810065+SateeshDara@users.noreply.github.com>
2018-09-29 14:19:16 +0530

    Add files via upload

---
+++ AnimationStack/AnimationStack/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  AnimationStack
+//
+//  Created by Sateesh Dara on 04/09/18.
+//  Copyright © 2018 Sateesh Dara. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+
+
+}
+
